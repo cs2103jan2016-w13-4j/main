d0a919425b39e80b3fca645dbabf845a08cb07e6
Kai Yuan <hedonist.ky@gmail.com>
2016-01-22 01:53:16 +0800

    Add documentation for Dummy class

--- src/test/Dummy.java
+++ src/test/Dummy.java
@@ -1,8 +1,16 @@
 package test;
 
+/**
+* Dummy
+* @author Kai Yuan
+* Description: Used to validate the functionality of the test utility
+*/
 public class Dummy
 {
 
+  /**
+   * @return true, alawys.
+   */
   public boolean exists()
   {
     return true;

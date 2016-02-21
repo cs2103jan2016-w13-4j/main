5a2097cf2a7a90fd9dcea11c7150ea39d8c9175c
Feixue <feixuehuang@yahoo.com>
2016-02-22 00:53:02 +0800

    edit styling error

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -11,85 +11,85 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Main extends Application {
-	
-	private Stage _primaryStage;
-	private Scene _JFDIScene;
-	private BorderPane _rootLayout;
-	private AnchorPane _listLayout;
-	// create a private controller variable using the Controller Class
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		
-		_primaryStage = primaryStage;
-		_primaryStage.setTitle("JFDI");
-		
-		setStageTransparent();
-		loadFonts(); // if any
-		initRootLayut();
-		showJFDIView();	
-		
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
-	
-	/**
-	 * LEVEL 1 of abstraction
-	 */
-	
-	private void setStageTransparent() {
-		_primaryStage.initStyle(StageStyle.TRANSPARENT);
-	}
+    private Stage primaryStage;
+    private Scene scene;
+    private BorderPane rootLayout;
+    private AnchorPane listLayout;
+    // create a private controller variable using the Controller Class
 
-	private void loadFonts() {
-		// Implement when team decides to specific font types that needs to be loaded
-		// Font font1 = Font.loadFont(Main.class.getResourceAsStream(<filepath e.g."resource/Oxygen regular.ttf">), <size>);
-        // Font font2 = Font.loadFont(Main.class.getResourceAsStream(<filepath e.g."resource/Oxygen regular.ttf">), <size>);
-	}	
+    @Override
+    public void start(Stage primaryStage) throws Exception {
 
-	private void initRootLayut() throws IOException {
-		
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("JFDI");
+
+        setStageTransparent();
+        loadFonts(); // if any
+        initRootLayut();
+        showJFDIView();	
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * LEVEL 1 of abstraction
+     */
+
+    private void setStageTransparent() {
+        this.primaryStage.initStyle(StageStyle.TRANSPARENT);
+    }
+
+    private void loadFonts() {
+        // Implement when team decides to specific font types that needs to be loaded
+        // Font font1 = Font.loadFont(Main.class.getResourceAsStream(
+        //                              <filepath e.g."resource/Oxygen regular.ttf">), <size>);
+        // Font font2 = Font.loadFont(Main.class.getResourceAsStream(
+        //                              <filepath e.g."resource/Oxygen regular.ttf">), <size>);
+    }
+
+    private void initRootLayut() throws IOException {
+        
 		FXMLLoader loader = new FXMLLoader();
 		loader.setLocation(null); // set URL of fxml file
-		_rootLayout = loader.load();
-		
+		rootLayout = loader.load();
+
+    }
+
+    private void showJFDIView() {
+        // Initialize UI
+        // Load View
+        // Initialize Controller
+        // Link Controller with UI and Main
+        // Link UI with Controller
+    }
+
+    /**
+     * LEVEL 2 of abstraction
+     */	
+
+    /**
+     * Get Methods for private variables
+     */
+    public Stage getPrimaryStage() {
+        return this.primaryStage;
 	}
 
-	private void showJFDIView() {
-		// Initialize UI
-		// Load View
-		// Initialize Controller
-		// Link Controller with UI and Main
-		// Link UI with Controller
-	}
-	
-	/**
-	 * LEVEL 2 of abstraction
-	 */
-	
-	
-	
-	/**
-	 * Get Methods for private variables
-	 */
-	public Stage getPrimaryStage() {
-		return _primaryStage;
-	}
-	
-	public Scene getJFDIScene() {
-		return _JFDIScene;
-	}
-	
-	public BorderPane getRootLayout() {
-		return _rootLayout;
-	}
-	
-	public AnchorPane getlistPane() {
-		return _listLayout;
-	}
-	
-	// create a get method for private controller
-	
+    public Scene getJFDIScene() {
+        return this.scene;
+    }
+
+    public BorderPane getRootLayout() {
+        return rootLayout;
+    }
+
+    public AnchorPane getlistPane() {
+        return listLayout;
+    }
+
+    // create a get method for private controller
+
 }
--- src/main/java/ui/MainUIController.java
+++
@@ -1,8 +0,0 @@
-package ui;
-
-public class MainUIController {
-	
-	// Initialize the controller object
-	// Deal with all UI elements, style setting, layout setting, listeners etc
-}
- 

402a94b7709ae592b60e77534a0a1b4b039d39a0
leonardhml <leonardhml@gmail.com>
2016-02-21 15:58:18 +0800

    Add Command class

--- src/main/java/parser/Parser.java
+++ src/main/java/parser/Parser.java
@@ -1,7 +1,13 @@
 package parser;
 
+import command.Command;
+
 public class Parser {
     public boolean test() {
         return true;
+    }
+
+    public Command parse(String inputString) {
+        return new Command();
     }
 }

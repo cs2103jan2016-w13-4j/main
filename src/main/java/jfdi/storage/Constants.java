b9b893dda5f2331bb45a3bea4274b648565e49da
Thng Kai Yuan <hedonist.ky@gmail.com>
2016-02-25 02:18:25 +0800

    Add constants used in TaskTest

--- src/main/java/jfdi/storage/Constants.java
+++ src/main/java/jfdi/storage/Constants.java
@@ -1,5 +1,8 @@
 package jfdi.storage;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -33,15 +36,25 @@ public class Constants {
     public static final String CHARSET = "UTF-8";
 
     // Test-specific constants
+    // Files
     public static final String TEST_DIRECTORY_PREFIX = "CS2103-JFDI";
     public static final String TEST_DIRECTORY_NAME = "Data";
     public static final String TEST_SUBDIRECTORY_NAME = "Data2";
     public static final String TEST_FILE_NAME = "test.txt";
     public static final String TEST_FILE_DATA = "hello world!";
+
+    // Aliases
     public static final String TEST_ALIAS = "somealias";
     public static final String TEST_COMMAND = "somecommand";
     public static final String TEST_ALIAS_2 = "somealias2";
     public static final String TEST_COMMAND_2 = "somecommand2";
+
+    // Tasks
+    public static final String TEST_TASK_DESCRIPTION = "my favourite description";
+    public static final String TEST_TASK_TAG_1 = "tagged";
+    public static final LocalDateTime TEST_TASK_STARTDATETIME = LocalDateTime.MIN;
+    public static final LocalDateTime TEST_TASK_ENDDATETIME = LocalDateTime.MAX;
+    public static final Duration TEST_TASK_REMINDER_DURATION = Duration.ZERO.plusDays(1);
 
     // List of records
     private static final List<Class<?>> RECORDS = Arrays.asList(Task.class, Alias.class);

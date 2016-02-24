dbd91445707c07818bf5d27eacc8c0706fcb1ab5
Thng Kai Yuan <hedonist.ky@gmail.com>
2016-02-24 22:41:28 +0800

    Create TestHelper

--- src/test/java/jfdi/test/storage/FileStorageTest.java
+++ src/test/java/jfdi/test/storage/FileStorageTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
 
 import java.io.File;
-import java.io.PrintWriter;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -114,7 +113,7 @@ public class FileStorageTest {
         initializeStorage();
         Path subdirectoryPath = Paths.get(testDirectoryString, Constants.TEST_SUBDIRECTORY_NAME);
         String subdirectoryString = subdirectoryPath.toString();
-        createInvalidDataFiles(subdirectoryString);
+        TestHelper.createInvalidDataFiles(subdirectoryString);
         fileStorageInstance.changeDirectory(subdirectoryString);
     }
 
@@ -123,7 +122,7 @@ public class FileStorageTest {
         initializeStorage();
         Path subdirectoryPath = Paths.get(testDirectoryString, Constants.TEST_SUBDIRECTORY_NAME);
         String subdirectoryString = subdirectoryPath.toString();
-        createValidDataFiles(subdirectoryString);
+        TestHelper.createValidDataFiles(subdirectoryString);
         try {
             fileStorageInstance.changeDirectory(subdirectoryString);
         } catch (Exception e) {
@@ -158,74 +157,13 @@ public class FileStorageTest {
      * This method creates valid data files in the test directory.
      */
     private void createValidDataFiles() {
-        createValidDataFiles(testDirectoryString);
-    }
-
-    /**
-     * This method creates valid data files in directoryPath
-     *
-     * @param directoryPath
-     *            the directory to create the data files in
-     */
-    private void createValidDataFiles(String directoryPath) {
-        createDataFilesWith(directoryPath, Constants.EMPTY_LIST_STRING);
+        TestHelper.createValidDataFiles(testDirectoryString);
     }
 
     /**
      * This method creates invalid data files in the test directory.
      */
     private void createInvalidDataFiles() {
-        createInvalidDataFiles(testDirectoryString);
-    }
-
-    /**
-     * This method creates invalid data files in directoryPath.
-     *
-     * @param directoryPath
-     *            the directory to create the data files in
-     */
-    private void createInvalidDataFiles(String directoryPath) {
-        createDataFilesWith(directoryPath, Constants.EMPTY_STRING);
-    }
-
-    /**
-     * This method creates data files in the specified directoryPath with the
-     * given data written into the data files.
-     *
-     * @param directoryPath
-     *            the path to the directory in which we create the data files
-     * @param data
-     *            the content that is to be written to the data files
-     */
-    private void createDataFilesWith(String directoryPath, String data) {
-        File parentDirectory = new File(directoryPath);
-        parentDirectory.mkdirs();
-
-        String[] dataFilenames = {Constants.FILENAME_TASK, Constants.FILENAME_ALIAS};
-        Path dataPath;
-        File dataFile;
-        for (String dataFilename : dataFilenames) {
-            dataPath = Paths.get(directoryPath, dataFilename);
-            dataFile = dataPath.toFile();
-            writeToFile(dataFile, data);
-        }
-    }
-
-    /**
-     * This method writes the given data to the given dataFile.
-     *
-     * @param dataFile
-     *            the file in which data is to be written into
-     * @param data
-     *            the data which we want to write into dataFile
-     */
-    private void writeToFile(File dataFile, String data) {
-        try {
-            PrintWriter writer = new PrintWriter(dataFile, Constants.CHARSET);
-            writer.println(data);
-            writer.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        TestHelper.createInvalidDataFiles(testDirectoryString);
     }
 }
---
+++ src/test/java/jfdi/test/storage/TestHelper.java
@@ -0,0 +1,72 @@
+package jfdi.test.storage;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import jfdi.storage.Constants;
+
+public class TestHelper {
+
+    /**
+     * This method creates valid data files in directoryPath
+     *
+     * @param directoryPath
+     *            the directory to create the data files in
+     */
+    public static void createValidDataFiles(String directoryPath) {
+        createDataFilesWith(directoryPath, Constants.EMPTY_LIST_STRING);
+    }
+
+    /**
+     * This method creates invalid data files in directoryPath.
+     *
+     * @param directoryPath
+     *            the directory to create the data files in
+     */
+    public static void createInvalidDataFiles(String directoryPath) {
+        createDataFilesWith(directoryPath, Constants.EMPTY_STRING);
+    }
+
+    /**
+     * This method creates data files in the specified directoryPath with the
+     * given data written into the data files.
+     *
+     * @param directoryPath
+     *            the path to the directory in which we create the data files
+     * @param data
+     *            the content that is to be written to the data files
+     */
+    public static void createDataFilesWith(String directoryPath, String data) {
+        File parentDirectory = new File(directoryPath);
+        parentDirectory.mkdirs();
+
+        String[] dataFilenames = {Constants.FILENAME_TASK, Constants.FILENAME_ALIAS};
+        Path dataPath;
+        File dataFile;
+        for (String dataFilename : dataFilenames) {
+            dataPath = Paths.get(directoryPath, dataFilename);
+            dataFile = dataPath.toFile();
+            writeToFile(dataFile, data);
+        }
+    }
+
+    /**
+     * This method writes the given data to the given dataFile.
+     *
+     * @param dataFile
+     *            the file in which data is to be written into
+     * @param data
+     *            the data which we want to write into dataFile
+     */
+    public static void writeToFile(File dataFile, String data) {
+        try {
+            PrintWriter writer = new PrintWriter(dataFile, Constants.CHARSET);
+            writer.println(data);
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}

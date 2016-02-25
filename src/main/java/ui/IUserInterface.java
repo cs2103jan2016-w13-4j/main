d14b6109fcf04878834b4e75c4dc75e412cdbd59
Feixue <feixuehuang@yahoo.com>
2016-02-26 00:35:24 +0800

    Add new line after method declaration

--- src/main/java/ui/IUserInterface.java
+++ src/main/java/ui/IUserInterface.java
@@ -7,8 +7,11 @@ import dummy.TaskDummy;
 public interface IUserInterface {
 
     void init();
+
     void displayWelcome();
+
     void processInput(String input);
+
     ArrayList<TaskDummy> getList();
     // Change "string" to the relevant task class once created
     void setController(MainController controller);

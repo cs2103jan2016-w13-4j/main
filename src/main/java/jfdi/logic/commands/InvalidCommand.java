247885450a73d9b15027a9c9d99c53f0323cd9bc
Liu Xinan <xinan@me.com>
2016-02-25 15:52:43 +0800

    Implement `InvalidCommand` and its tests

---
+++ src/main/java/jfdi/logic/commands/InvalidCommand.java
@@ -0,0 +1,66 @@
+package jfdi.logic.commands;
+
+import jfdi.logic.interfaces.AbstractCommand;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+
+/**
+ * @author Liu Xinan
+ */
+public class InvalidCommand extends AbstractCommand {
+
+    private static ArrayList<Consumer<InvalidCommand>> successHooks = new ArrayList<>();
+    private static ArrayList<Consumer<InvalidCommand>> failureHooks = new ArrayList<>();
+
+    private InvalidCommand(Builder builder) {}
+
+    public static class Builder {
+
+        public InvalidCommand build() {
+            return new InvalidCommand(this);
+        }
+
+    }
+
+    @Override
+    public void execute() {
+        // Invalid command always fail.
+        onFailure();
+    }
+
+    @Override
+    protected void onSuccess() {
+        for (Consumer<InvalidCommand> hook : successHooks) {
+            hook.accept(this);
+        }
+    }
+
+    @Override
+    protected void onFailure() {
+        for (Consumer<InvalidCommand> hook : failureHooks) {
+            hook.accept(this);
+        }
+    }
+
+    /**
+     * Add a hook to be run when the command executes successfully.
+     * The hook will be passed the command object as argument.
+     *
+     * @param hook Command hook to be run upon success
+     */
+    public static void addSuccessHook(Consumer<InvalidCommand> hook) {
+        successHooks.add(hook);
+    }
+
+    /**
+     * Add a hook to be run when the command encounters error.
+     * The hook will be passed the command object as argument.
+     *
+     * @param hook Command hook to be run upon failure
+     */
+    public static void addFailureHook(Consumer<InvalidCommand> hook) {
+        failureHooks.add(hook);
+    }
+
+}

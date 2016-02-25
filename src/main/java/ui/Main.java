5805f24f9137089bd9d7ec91d9645b56ff82b752
Feixue <feixuehuang@yahoo.com>
2016-02-25 20:59:51 +0800

    Update UI components

--- src/main/java/ui/Main.java
+++ src/main/java/ui/Main.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -16,7 +17,7 @@ public class Main extends Application {
     private Scene scene;
     private BorderPane rootLayout;
     private AnchorPane listLayout;
-    // create a private controller variable using the Controller Class
+    private MainController controller;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -26,8 +27,8 @@ public class Main extends Application {
 
         setStageTransparent();
         loadFonts(); // if any
-        initRootLayut();
-        showView();
+        initRootLayout();
+        initView();
 
     }
 
@@ -35,9 +36,9 @@ public class Main extends Application {
         launch(args);
     }
 
-    /**
-     * LEVEL 1 of abstraction
-     */
+    /***************************
+     *** LEVEL 1 Abstraction ***
+     ***************************/
 
     private void setStageTransparent() {
         this.primaryStage.initStyle(StageStyle.TRANSPARENT);
@@ -51,25 +52,48 @@ public class Main extends Application {
         //                              <filepath e.g."resource/Oxygen regular.ttf">), <size>);
     }
 
-    private void initRootLayut() throws IOException {
+    private void initRootLayout() throws IOException {
 
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(null); // set URL of fxml file
-        rootLayout = loader.load();
+        rootLayout = (BorderPane) loader.load();
+        
+        // Display scene with root layout
+        Scene scene = new Scene(rootLayout);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        this.scene = scene;
 
     }
 
-    private void showView() {
+    private void initView() throws IOException {
         // Initialize UI
+        IUserInterface userInterface = new UserInterface();
+        userInterface.init();
+        
         // Load View
-        // Initialize Controller
-        // Link Controller with UI and Main
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(null); // set URL of fxml file
+        AnchorPane appView = (AnchorPane) loader.load();
+        rootLayout.setCenter(appView);
+        
+        // Initialize Controller (set up MainController Class first)
+        controller = loader.getController();
+        controller.setStage(primaryStage);
+        
+        // Link Controller with UI and Main (set up MainController Class first)
+        controller.setUI(userInterface);
+        controller.setMainApp(this);
+        
         // Link UI with Controller
+        userInterface.setController(controller);
+        userInterface.displayWelcome();
     }
 
-    /**
-     * LEVEL 2 of abstraction
-     */
+    /***************************
+     *** LEVEL 2 Abstraction ***
+     ***************************/
 
     /**
      * Get Methods for private variables
@@ -83,12 +107,14 @@ public class Main extends Application {
     }
 
     public BorderPane getRootLayout() {
-        return rootLayout;
+        return this.rootLayout;
     }
 
     public AnchorPane getlistPane() {
-        return listLayout;
+        return this.listLayout;
     }
 
-    // create a get method for private controller
+    public MainController getController() {
+        return this.controller;
+    }
 }

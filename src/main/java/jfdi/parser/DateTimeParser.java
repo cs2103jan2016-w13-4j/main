a2138b4c348514c85f9e6d8544c17f565f7f806a
leonardhml <leonardhml@gmail.com>
2016-02-27 00:43:55 +0800

    Comment DateTimeParser

--- src/main/java/jfdi/parser/DateTimeParser.java
+++ src/main/java/jfdi/parser/DateTimeParser.java
@@ -8,6 +8,16 @@ import java.util.List;
 import com.joestelmach.natty.DateGroup;
 import com.joestelmach.natty.Parser;
 
+/**
+ * DateTimeParser is a class used to parse a string input into one a list of
+ * LocalDateTime objects it represents. It supports parsing strings of different
+ * formats. For example, it can parse "by 23rd February 2015" or "from 23/02/15
+ * 2359hrs to 29/03/16 09:40pm". For an idea of the format of String parseable,
+ * see parser.Constants.java.
+ *
+ * @author Leonard Hio
+ *
+ */
 public class DateTimeParser {
     private static DateTimeParser dateTimeParser;
 
@@ -16,6 +26,17 @@ public class DateTimeParser {
                 : dateTimeParser;
     }
 
+    /**
+     * This method parses the given input string into a list of LocalDateTime
+     * objects.
+     *
+     * @param input
+     *            a string that should match the date-time format listed in
+     *            parser.Constants.java.
+     * @return a list of localDateTime objects, parsed from the input string.
+     *         The list can be of size 0, which indicates the input string
+     *         contains no parseable date time String.
+     */
     public List<LocalDateTime> parseDateTime(String input) {
         Parser parser = new Parser();
         List<DateGroup> dateTimeList = parser.parse(input);
@@ -29,6 +50,14 @@ public class DateTimeParser {
         return ldtList;
     }
 
+    /**
+     * This method reformats a given Date object to a LocalDateTime object. The
+     * zone is taken to be the one found in the system.
+     *
+     * @param d
+     *            a Date object
+     * @return the LocalDateTime object formatted from the Date object.
+     */
     public LocalDateTime getLocalDateTimeFromDate(Date d) {
         return LocalDateTime.ofInstant(d.toInstant(), Constants.ZONE_ID);
     }

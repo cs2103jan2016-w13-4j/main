ebec294c6f9efee49002f9214caf98d86e8a1ace
Liu Xinan <xinan@me.com>
2016-03-05 23:17:06 +0800

    Update `ExitCommand` and add corresponding events

--- src/main/java/jfdi/logic/commands/ExitCommand.java
+++ src/main/java/jfdi/logic/commands/ExitCommand.java
@@ -1,6 +1,7 @@
 package jfdi.logic.commands;
 
 import jfdi.CoverageIgnore;
+import jfdi.logic.events.ExitCalledEvent;
 import jfdi.logic.interfaces.Command;
 
 import java.util.ArrayList;
@@ -10,9 +11,6 @@ import java.util.function.Consumer;
  * @author Liu Xinan
  */
 public class ExitCommand extends Command {
-
-    private static ArrayList<Consumer<ExitCommand>> successHooks = new ArrayList<>();
-    private static ArrayList<Consumer<ExitCommand>> failureHooks = new ArrayList<>();
 
     private ExitCommand(Builder builder) {}
 
@@ -27,42 +25,8 @@ public class ExitCommand extends Command {
     @Override
     public void execute() {
         // Nothing needs to be done.
-        // Call success hooks and UI will exit.
-        onSuccess();
+        // Post an event to notify UI to exit.
+        eventBus.post(new ExitCalledEvent());
     }
 
-    @Override
-    protected void onSuccess() {
-        for (Consumer<ExitCommand> hook : successHooks) {
-            hook.accept(this);
-        }
-    }
-
-    @Override
-    @CoverageIgnore
-    protected void onFailure() {
-        for (Consumer<ExitCommand> hook : failureHooks) {
-            hook.accept(this);
-        }
-    }
-
-    /**
-     * Add a hook to be run when the command executes successfully.
-     * The hook will be passed the command object as argument.
-     *
-     * @param hook Command hook to be run upon success
-     */
-    public static void addSuccessHook(Consumer<ExitCommand> hook) {
-        successHooks.add(hook);
-    }
-
-    /**
-     * Add a hook to be run when the command encounters error.
-     * The hook will be passed the command object as argument.
-     *
-     * @param hook Command hook to be run upon failure
-     */
-    public static void addFailureHook(Consumer<ExitCommand> hook) {
-        failureHooks.add(hook);
-    }
 }

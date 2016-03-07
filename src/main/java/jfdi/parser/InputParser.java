a0299bd7ed1806d1655135d95a56f2519e59b01c
leonardhml <leonardhml@gmail.com>
2016-03-08 00:12:43 +0800

    Add comments and make code more SLAP friendly

--- src/main/java/jfdi/parser/InputParser.java
+++ src/main/java/jfdi/parser/InputParser.java
@@ -1,9 +1,7 @@
 package jfdi.parser;
 
-import java.util.Arrays;
-import java.util.List;
-
 import jfdi.logic.interfaces.Command;
+import jfdi.parser.Constants.CommandType;
 import jfdi.parser.commandparsers.AddCommandParser;
 import jfdi.parser.commandparsers.DeleteCommandParser;
 import jfdi.parser.commandparsers.ListCommandParser;
@@ -34,10 +32,75 @@ public class InputParser implements IParser {
         if (!isValidInput(input)) {
             throw new InvalidInputException(input);
         }
-        List<String> userArguments = getUserArgumentsFromInput(input);
-        String userActionAsString = getActionAsString(userArguments);
-        Command userCommand = getCommand(userActionAsString, input);
+
+        // / input is guaranteed to be at least one word long
+        String firstWord = getFirstWord(input);
+        CommandType commandType = getCommandType(firstWord);
+        Command userCommand = getCommand(commandType, input);
         return userCommand;
+    }
+
+    /**
+     * This method returns the first word of the input String.
+     *
+     * @param input
+     *            a String from which the first word is to be returned.
+     * @return the first word of the input String.
+     */
+    private String getFirstWord(String input) {
+        assert isValidInput(input);
+        return input.trim().split(Constants.REGEX_WHITESPACE)[0];
+    }
+
+    /**
+     * This method returns the CommandType associated with the input String.
+     *
+     * @param input
+     *            a String interpretation of a CommandType.
+     * @return a CommandType enum.
+     */
+    private CommandType getCommandType(String input) {
+        assert input.split(Constants.REGEX_WHITESPACE).length == 1;
+        if (input.matches(Constants.REGEX_ADD)) {
+            return Constants.CommandType.add;
+        } else if (input.matches(Constants.REGEX_LIST)) {
+            return Constants.CommandType.list;
+        } else if (input.matches(Constants.REGEX_DELETE)) {
+            return Constants.CommandType.delete;
+        } else if (input.matches(Constants.REGEX_RENAME)) {
+            return Constants.CommandType.rename;
+        } else if (input.matches(Constants.REGEX_RESCHEDULE)) {
+            return Constants.CommandType.reschedule;
+        } else {
+            return Constants.CommandType.add;
+        }
+
+    }
+
+    /**
+     * This method gets the Command object associated with the user's input.
+     *
+     * @param commandType
+     *            the CommandType of the user's input.
+     * @param input
+     *            the user's input itself.
+     * @return a Command object that was built from the user's input.
+     */
+    private Command getCommand(CommandType commandType, String input) {
+        switch (commandType) {
+            case add:
+                return AddCommandParser.getInstance().build(input);
+            case list:
+                return ListCommandParser.getInstance().build(input);
+            case delete:
+                return DeleteCommandParser.getInstance().build(input);
+            case rename:
+                return RenameCommandParser.getInstance().build(input);
+            case reschedule:
+                return RescheduleCommandParser.getInstance().build(input);
+            default:
+                return AddCommandParser.getInstance().build(input);
+        }
     }
 
     /**
@@ -50,36 +113,5 @@ public class InputParser implements IParser {
      */
     private boolean isValidInput(String input) {
         return !(input.isEmpty() || input.trim().isEmpty());
-    }
-
-    private List<String> getUserArgumentsFromInput(String userInput) {
-        String[] userArgumentsAsArray = userInput
-                .split(Constants.REGEX_WHITESPACE);
-        return getList(userArgumentsAsArray);
-    }
-
-    private String getActionAsString(List<String> userArguments) {
-        String userActionAsString = userArguments.get(Constants.INDEX_ACTION);
-        return userActionAsString;
-    }
-
-    private Command getCommand(String userActionAsString, String input) {
-        if (userActionAsString.matches(Constants.REGEX_ADD)) {
-            return AddCommandParser.getInstance().build(input);
-        } else if (userActionAsString.matches(Constants.REGEX_LIST)) {
-            return ListCommandParser.getInstance().build(input);
-        } else if (userActionAsString.matches(Constants.REGEX_DELETE)) {
-            return DeleteCommandParser.getInstance().build(input);
-        } else if (userActionAsString.matches(Constants.REGEX_RENAME)) {
-            return RenameCommandParser.getInstance().build(input);
-        } else if (userActionAsString.matches(Constants.REGEX_RESCHEDULE)) {
-            return RescheduleCommandParser.getInstance().build(input);
-        } else {
-            return AddCommandParser.getInstance().build(input);
-        }
-    }
-
-    public List<String> getList(String[] array) {
-        return Arrays.asList(array);
     }
 }

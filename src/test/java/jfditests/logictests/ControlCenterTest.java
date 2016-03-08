9e0ba9e41ded36748bce26b57a679e9e46224833
leonardhml <leonardhml@gmail.com>
2016-03-08 22:33:43 +0800

    remove checkstyle for magic numbers. Integrate Parser into Logic

--- src/test/java/jfditests/logictests/ControlCenterTest.java
+++ src/test/java/jfditests/logictests/ControlCenterTest.java
@@ -1,10 +1,16 @@
 package jfditests.logictests;
 
-import dummy.InputParser;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 import jfdi.logic.ControlCenter;
 import jfdi.logic.commands.ExitCommand;
 import jfdi.logic.commands.InvalidCommand;
 import jfdi.logic.commands.ListCommand;
+import jfdi.parser.InputParser;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -12,22 +18,21 @@ import org.mockito.Mock;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import static org.junit.Assert.assertSame;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.verify;
-import static org.powermock.api.mockito.PowerMockito.*;
-
 /**
  * @author Liu Xinan
  */
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({InputParser.class})
+@PrepareForTest({ InputParser.class })
 public class ControlCenterTest {
 
-    @Mock private InputParser parser;
-    @Mock private ListCommand ls;
-    @Mock private ExitCommand exit;
-    @Mock private InvalidCommand lol;
+    @Mock
+    private InputParser parser;
+    @Mock
+    private ListCommand ls;
+    @Mock
+    private ExitCommand exit;
+    @Mock
+    private InvalidCommand lol;
 
     private ControlCenter cc = ControlCenter.getInstance();
 

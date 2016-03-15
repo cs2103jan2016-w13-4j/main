8e279bd2f5fca236aea109875aec1761422bd4f9
Liu Xinan <xinan@me.com>
2016-03-15 15:23:25 +0800

    Implement `SearchCommand`

---
+++ src/main/java/jfdi/logic/events/SearchDoneEvent.java
@@ -0,0 +1,29 @@
+package jfdi.logic.events;
+
+import jfdi.storage.apis.TaskAttributes;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+ * @author Liu Xinan
+ */
+public class SearchDoneEvent {
+
+    private ArrayList<TaskAttributes> results;
+    private HashSet<String> keywords;
+
+    public SearchDoneEvent(ArrayList<TaskAttributes> results, HashSet<String> keywords) {
+        this.results = results;
+        this.keywords = keywords;
+    }
+
+    public ArrayList<TaskAttributes> getResults() {
+        return results;
+    }
+
+    public HashSet<String> getKeywords() {
+        return keywords;
+    }
+
+}

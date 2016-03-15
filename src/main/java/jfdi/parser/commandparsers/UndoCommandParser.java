d3982c9d34f4148e812e2765f0d9383e371800e5
leonardhml <leonardhml@gmail.com>
2016-03-16 01:08:15 +0800

    Implement undo command parser and stub

--- src/main/java/jfdi/parser/commandparsers/UndoCommandParser.java
+++ src/main/java/jfdi/parser/commandparsers/UndoCommandParser.java
@@ -1,13 +1,32 @@
 package jfdi.parser.commandparsers;
 
+import jfdi.logic.commands.UndoCommandStub.Builder;
 import jfdi.logic.interfaces.Command;
 
+/**
+ * This class parses the Undo command input by the user. The Undo command is in
+ * the format {Undo identifier} i.e. a singular word.
+ *
+ * @author Leonard Hio
+ *
+ */
 public class UndoCommandParser extends AbstractCommandParser {
+    public static UndoCommandParser instance;
+
+    private UndoCommandParser() {
+    }
+
+    public static UndoCommandParser getInstance() {
+        if (instance == null) {
+            return instance = new UndoCommandParser();
+        }
+        return instance;
+    }
 
     @Override
     public Command build(String input) {
-        // TODO Auto-generated method stub
-        return null;
+        Builder builder = new Builder();
+        return builder.build();
     }
 
 }

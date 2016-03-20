8e1399070f3f57e4d5c8746991647e855b010b48
Thng Kai Yuan <hedonist.ky@gmail.com>
2016-03-20 13:33:42 +0800

    Refactor Storage tests

--- src/test/java/jfdi/test/storage/AliasTest.java
+++ src/test/java/jfdi/test/storage/AliasTest.java
@@ -10,6 +10,7 @@ public class AliasTest {
 
     @Test
     public void testAliasConstructorAndGetters() {
+        // We create an alias and check that the getters return the correct values
         Alias alias = new Alias(Constants.TEST_ALIAS, Constants.TEST_COMMAND);
         assertEquals(alias.getAlias(), Constants.TEST_ALIAS);
         assertEquals(alias.getCommand(), Constants.TEST_COMMAND);

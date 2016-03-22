23a65212ac0369ebd888226b43a3a8c72d021db6
leonardhml <leonardhml@gmail.com>
2016-03-22 10:55:12 +0800

    Add support for tonight, this evening, etc

--- src/main/java/jfdi/parser/Constants.java
+++ src/main/java/jfdi/parser/Constants.java
@@ -60,8 +60,9 @@ public class Constants {
         + "(week|wk)(s)?|" + "(month|mth)(s)?|" + "(year|yr))(s)?";
     public static final String REGEX_TIME_MILITARY = "(?i)([0-1][0-9]|[2][0-4])[.:]?[0-5][\\d]([h]([r][s]?)?)";
     public static final String REGEX_TIME_NORMAL = "((?i)0?[1-9]|1[0-2])[.:]?([0-5][0-9])?[ :]?([a|p][m])";
+    public static final String REGEX_TIME_RELATIVE = "((?i)(morning|afternoon|evening|night|midnight))";
 
-    public static final String REGEX_RELATIVE_DATE_1 = "(?i)(tomorrow|yesterday|today|now)";
+    public static final String REGEX_RELATIVE_DATE_1 = "(?i)(tomorrow|yesterday|today|now|tonight)";
     public static final String REGEX_RELATIVE_DATE_2 = "(?i)((next |this )?("
         + REGEX_DATE_ATTRIBUTES + "|" + REGEX_DAYS_TEXTUAL + "))";
     public static final String REGEX_RELATIVE_DATE_3 = "((\\d+ )"
@@ -84,7 +85,7 @@ public class Constants {
         + REGEX_ABSOLUTE_DATE_DDMMYYYY + "|" + REGEX_ABSOLUTE_DATE_DDMONTHYYYY
         + "|" + REGEX_RELATIVE_DATE + ")";
     public static final String REGEX_TIME_FORMAT = "(" + REGEX_TIME_MILITARY
-        + "|" + REGEX_TIME_NORMAL + ")";
+        + "|" + REGEX_TIME_NORMAL + "|" + REGEX_TIME_RELATIVE + ")";
     public static final String REGEX_DATE_TIME_FORMAT_DATE_FIRST = "("
         + REGEX_DATE_FORMAT + ",?[ ]" + REGEX_TIME_FORMAT + ")";
     public static final String REGEX_DATE_TIME_FORMAT_DATE_FIRST_WITH_NAMED_GROUPS = "((?<date>"
@@ -199,7 +200,8 @@ public class Constants {
 
     public static void main(String[] args) {
 
-        System.out.println("next week".matches(REGEX_DATE_TIME_IDENTIFIER));
+        System.out.println("from 3 day later night to 5 days later morning"
+            .matches(REGEX_DATE_TIME_IDENTIFIER));
 
     }
 }

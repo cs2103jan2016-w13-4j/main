aa274c931fced3dd21c8cf4ed30719e351514273
leonardhml <leonardhml@gmail.com>
2016-03-29 14:22:09 +0800

    Add Exit command parser tests

---
+++ src/test/java/jfdi/test/parser/ExitCommandParserTest.java
@@ -0,0 +1,62 @@
+// @@author A0127393B
+
+package jfdi.test.parser;
+
+import static org.junit.Assert.assertSame;
+import jfdi.logic.commands.ExitCommand;
+import jfdi.logic.commands.InvalidCommand;
+import jfdi.logic.interfaces.Command;
+import jfdi.parser.commandparsers.AbstractCommandParser;
+import jfdi.parser.commandparsers.ExitCommandParser;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ExitCommandParserTest {
+
+    AbstractCommandParser parser = ExitCommandParser.getInstance();
+
+    @Before
+    public void init() {
+        parser = ExitCommandParser.getInstance();
+    }
+
+    @Test
+    public void testGetInstance() throws Exception {
+        assertSame(parser, ExitCommandParser.getInstance());
+    }
+
+    @Test
+    public void testValidCommand() {
+        Command command = parser.build("exit");
+        Assert.assertTrue(command instanceof ExitCommand);
+        command = parser.build("Exit");
+        Assert.assertTrue(command instanceof ExitCommand);
+        command = parser.build("exit");
+        Assert.assertTrue(command instanceof ExitCommand);
+    }
+
+    @Test
+    public void testInvalidCommand() {
+        // Multiple words
+        Command command = parser.build("exit via the back door");
+        Assert.assertTrue(command instanceof InvalidCommand);
+
+        // Boundary case: wrong command
+        command = parser.build("add 1-8");
+        Assert.assertTrue(command instanceof InvalidCommand);
+
+        // Boundary case: empty string
+        command = parser.build("");
+        Assert.assertTrue(command instanceof InvalidCommand);
+
+        // Boundary case: null
+        try {
+            command = parser.build(null);
+        } catch (AssertionError e) {
+            Assert.assertTrue(true);
+        }
+    }
+
+}

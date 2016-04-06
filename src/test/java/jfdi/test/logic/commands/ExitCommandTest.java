7d969b7f3c2c40e17348025e757dbedb8f80dc2a
Liu Xinan <xinan@me.com>
2016-04-06 20:30:02 +0800

    Add test for `ExitCommand`

---
+++ src/test/java/jfdi/test/logic/commands/ExitCommandTest.java
@@ -0,0 +1,34 @@
+package jfdi.test.logic.commands;
+
+import jfdi.logic.commands.ExitCommand;
+import jfdi.logic.events.ExitCalledEvent;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Liu Xinan
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ExitCommandTest extends CommonCommandTest {
+
+    @Test
+    public void execute() throws Exception {
+        ExitCommand command = new ExitCommand.Builder().build();
+
+        command.execute();
+
+        verify(eventBus).post(any(ExitCalledEvent.class));
+    }
+
+    @Test
+    public void undo() throws Exception {
+        ExitCommand command = new ExitCommand.Builder().build();
+
+        thrown.expect(AssertionError.class);
+        command.undo();
+    }
+
+}

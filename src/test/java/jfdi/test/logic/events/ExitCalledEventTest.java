6977a9b6b50b38dac32751b74b0afa186ed79cc1
Liu Xinan <xinan@me.com>
2016-04-06 18:48:53 +0800

    Add test for `ExitCalledEvent`

---
+++ src/test/java/jfdi/test/logic/events/ExitCalledEventTest.java
@@ -0,0 +1,17 @@
+package jfdi.test.logic.events;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Xinan
+ */
+public class ExitCalledEventTest {
+
+    @Test
+    public void nothingToTest() {
+        assertTrue(true);
+    }
+
+}

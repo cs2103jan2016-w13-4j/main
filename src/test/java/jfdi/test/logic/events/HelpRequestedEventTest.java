3f4cd8a04ecae641d4669df1aa71f29fc356a14a
Liu Xinan <xinan@me.com>
2016-04-06 18:53:17 +0800

    Add test for `HelpRequestedEvent`

---
+++ src/test/java/jfdi/test/logic/events/HelpRequestedEventTest.java
@@ -0,0 +1,18 @@
+// @@author A0130195M
+package jfdi.test.logic.events;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Liu Xinan
+ */
+public class HelpRequestedEventTest {
+
+    @Test
+    public void nothingToTest() {
+        assertTrue(true);
+    }
+
+}

e7be7139dd69a2ace84a56b1f49fa000b4a6d475
Liu Xinan <xinan@me.com>
2016-04-06 20:32:30 +0800

    Add test for `HelpCommand`

---
+++ src/test/java/jfdi/test/logic/commands/HelpCommandTest.java
@@ -0,0 +1,34 @@
+package jfdi.test.logic.commands;
+
+import jfdi.logic.commands.HelpCommand;
+import jfdi.logic.events.HelpRequestedEvent;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Liu Xinan
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class HelpCommandTest extends CommonCommandTest {
+
+    @Test
+    public void execute() throws Exception {
+        HelpCommand command = new HelpCommand.Builder().build();
+
+        command.execute();
+
+        verify(eventBus).post(any(HelpRequestedEvent.class));
+    }
+
+    @Test
+    public void undo() throws Exception {
+        HelpCommand command = new HelpCommand.Builder().build();
+
+        thrown.expect(AssertionError.class);
+        command.undo();
+    }
+
+}

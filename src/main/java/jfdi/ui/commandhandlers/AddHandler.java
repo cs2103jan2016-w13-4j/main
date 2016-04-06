60b330a613d417d61342f5adec3ca9be06525879
Feixue <feixuehuang@yahoo.com>
2016-04-06 09:27:18 +0800

    Minor changes

--- src/main/java/jfdi/ui/commandhandlers/AddHandler.java
+++ src/main/java/jfdi/ui/commandhandlers/AddHandler.java
@@ -61,9 +61,9 @@ public class AddHandler extends CommandHandler {
 
         controller.updateNotiBubbles();
         int index = findCurrentIndex(task);
+        controller.listMain.scrollTo(index); // not working properly for overdue, upcoming and search when index > 5
         logger.fine(String.format(Constants.LOG_ADDED_SUCCESS, task.getId()));
         controller.relayFb(String.format(Constants.CMD_SUCCESS_ADDED, index, task.getDescription()), MsgType.SUCCESS);
-        controller.listMain.scrollTo(index);
     }
 
     @Subscribe

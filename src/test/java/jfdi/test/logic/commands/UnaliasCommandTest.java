ef52b7ae46ef6dd07143c28a32be18912a0d3f51
Liu Xinan <xinan@me.com>
2016-04-07 11:51:20 +0800

    Add test for `UnaliasCommand`

---
+++ src/test/java/jfdi/test/logic/commands/UnaliasCommandTest.java
@@ -0,0 +1,84 @@
+package jfdi.test.logic.commands;
+
+import jfdi.logic.commands.UnaliasCommand;
+import jfdi.logic.events.UnaliasFailedEvent;
+import jfdi.storage.apis.AliasAttributes;
+import jfdi.storage.exceptions.InvalidAliasException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyCollectionOf;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Liu Xinan
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class UnaliasCommandTest extends CommonCommandTest {
+
+    @Test
+    public void testBuilder() throws Exception {
+        UnaliasCommand command = new UnaliasCommand.Builder()
+            .setAlias("ls")
+            .build();
+
+        assertEquals("ls", command.getAlias());
+    }
+
+    @Test
+    public void testExecute_successful() throws Exception {
+        UnaliasCommand command = new UnaliasCommand.Builder()
+            .setAlias("ls")
+            .build();
+
+        command.execute();
+
+        verify(aliasDb).destroy("ls");
+        verify(aliasDb).getAll();
+        verify(parser).setAliases(anyCollectionOf(AliasAttributes.class));
+    }
+
+    @Test
+    public void testExecute_unsuccessful() throws Exception {
+        doThrow(InvalidAliasException.class).when(aliasDb).destroy("ls");
+
+        UnaliasCommand command = new UnaliasCommand.Builder()
+            .setAlias("ls")
+            .build();
+
+        command.execute();
+
+        verify(aliasDb).destroy("ls");
+        verifyNoMoreInteractions(aliasDb);
+        verifyZeroInteractions(parser);
+        verify(eventBus).post(any(UnaliasFailedEvent.class));
+    }
+
+    @Test
+    public void testUndo_successful() throws Exception {
+        UnaliasCommand command = new UnaliasCommand.Builder()
+            .setAlias("ls")
+            .build();
+
+        command.undo();
+
+        verify(aliasDb).undestroy("ls");
+        verify(aliasDb).getAll();
+        verify(parser).setAliases(anyCollectionOf(AliasAttributes.class));
+    }
+
+    @Test
+    public void testUndo_unsuccessful() throws Exception {
+        doThrow(InvalidAliasException.class).when(aliasDb).undestroy("ls");
+
+        UnaliasCommand command = new UnaliasCommand.Builder()
+            .setAlias("ls")
+            .build();
+
+        thrown.expect(AssertionError.class);
+        command.undo();
+    }
+
+}

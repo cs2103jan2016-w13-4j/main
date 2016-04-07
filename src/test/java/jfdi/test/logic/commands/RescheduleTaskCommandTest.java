784e522f3648a04d76ea8057e76d79b520f36492
Liu Xinan <xinan@me.com>
2016-04-07 14:25:41 +0800

    Improve `RescheduleTaskCommandTest`

--- src/test/java/jfdi/test/logic/commands/RescheduleTaskCommandTest.java
+++ src/test/java/jfdi/test/logic/commands/RescheduleTaskCommandTest.java
@@ -46,7 +46,7 @@ public class RescheduleTaskCommandTest extends CommonCommandTest {
     }
 
     @Test
-    public void testExecute_successful() throws Exception {
+    public void testExecute_nonShifting_successful() throws Exception {
         TaskAttributes task = mock(TaskAttributes.class);
         when(ui.getTaskId(1)).thenReturn(1);
         when(taskDb.getById(1)).thenReturn(task);
@@ -67,6 +67,156 @@ public class RescheduleTaskCommandTest extends CommonCommandTest {
         verify(task).setStartDateTime(any(LocalDateTime.class));
         verify(task).setEndDateTime(any(LocalDateTime.class));
         verify(task).save();
+    }
+
+    @Test
+    public void testExecute_shiftingFloatingTask_successful() throws Exception {
+        TaskAttributes floating = mock(TaskAttributes.class);
+        when(floating.getStartDateTime()).thenReturn(null);
+        when(floating.getEndDateTime()).thenReturn(null);
+        when(ui.getTaskId(1)).thenReturn(1);
+        when(taskDb.getById(1)).thenReturn(floating);
+
+        LocalDateTime shiftedDateTime = LocalDateTime.now().plusDays(1);
+
+        RescheduleTaskCommand command = new RescheduleTaskCommand.Builder()
+            .setId(1)
+            .setShiftedDateTime(shiftedDateTime)
+            .setShiftedDateSpecified(true)
+            .setShiftedTimeSpecified(true)
+            .build();
+
+        command.execute();
+
+        verify(floating).setStartDateTime(shiftedDateTime);
+        verify(floating).setEndDateTime(null);
+        verify(floating).save();
+    }
+
+    @Test
+    public void testExecute_shiftingPointTask_successful() throws Exception {
+        TaskAttributes point = mock(TaskAttributes.class);
+        when(point.getStartDateTime()).thenReturn(LocalDateTime.now());
+        when(point.getEndDateTime()).thenReturn(null);
+        when(ui.getTaskId(2)).thenReturn(2);
+        when(taskDb.getById(2)).thenReturn(point);
+
+        LocalDateTime shiftedDateTime = LocalDateTime.now().plusDays(1);
+
+        RescheduleTaskCommand command = new RescheduleTaskCommand.Builder()
+            .setId(2)
+            .setShiftedDateTime(shiftedDateTime)
+            .setShiftedDateSpecified(true)
+            .setShiftedTimeSpecified(true)
+            .build();
+
+        command.execute();
+
+        verify(point).setStartDateTime(shiftedDateTime);
+        verify(point).setEndDateTime(null);
+        verify(point).save();
+    }
+
+    @Test
+    public void testExecute_shiftingDeadlineTask_successful() throws Exception {
+        TaskAttributes deadline = mock(TaskAttributes.class);
+        when(deadline.getStartDateTime()).thenReturn(null);
+        when(deadline.getEndDateTime()).thenReturn(LocalDateTime.now());
+        when(ui.getTaskId(3)).thenReturn(3);
+        when(taskDb.getById(3)).thenReturn(deadline);
+
+        LocalDateTime shiftedDateTime = LocalDateTime.now().plusDays(1);
+
+        RescheduleTaskCommand command = new RescheduleTaskCommand.Builder()
+            .setId(3)
+            .setShiftedDateTime(shiftedDateTime)
+            .setShiftedDateSpecified(true)
+            .setShiftedTimeSpecified(true)
+            .build();
+
+        command.execute();
+
+        verify(deadline).setStartDateTime(null);
+        verify(deadline).setEndDateTime(shiftedDateTime);
+        verify(deadline).save();
+    }
+
+    @Test
+    public void testExecute_shiftingEventTask_bothDateTimeSpecified_successful() throws Exception {
+        TaskAttributes event = mock(TaskAttributes.class);
+        when(event.getStartDateTime()).thenReturn(LocalDateTime.now());
+        when(event.getEndDateTime()).thenReturn(LocalDateTime.now().plusHours(1));
+        when(ui.getTaskId(4)).thenReturn(4);
+        when(taskDb.getById(4)).thenReturn(event);
+
+        LocalDateTime shiftedDateTime = LocalDateTime.now().plusDays(1);
+
+        RescheduleTaskCommand command = new RescheduleTaskCommand.Builder()
+            .setId(4)
+            .setShiftedDateTime(shiftedDateTime)
+            .setShiftedDateSpecified(true)
+            .setShiftedTimeSpecified(true)
+            .build();
+
+        command.execute();
+
+        verify(event).setStartDateTime(shiftedDateTime);
+        verify(event).setEndDateTime(shiftedDateTime.plusHours(1));
+        verify(event).save();
+    }
+
+    @Test
+    public void testExecute_shiftingEventTask_onlyDateSpecified_successful() throws Exception {
+        LocalDateTime oldStartDateTime = LocalDateTime.now();
+        LocalDateTime oldEndDateTime = oldStartDateTime.plusHours(1);
+
+        TaskAttributes event = mock(TaskAttributes.class);
+        when(event.getStartDateTime()).thenReturn(oldStartDateTime);
+        when(event.getEndDateTime()).thenReturn(oldEndDateTime);
+        when(ui.getTaskId(4)).thenReturn(4);
+        when(taskDb.getById(4)).thenReturn(event);
+
+        LocalDateTime shiftedDateTime = LocalDateTime.now().plusDays(1);
+
+        RescheduleTaskCommand command = new RescheduleTaskCommand.Builder()
+            .setId(4)
+            .setShiftedDateTime(shiftedDateTime)
+            .setShiftedDateSpecified(true)
+            .setShiftedTimeSpecified(false)
+            .build();
+
+        command.execute();
+
+        verify(event).setStartDateTime(oldStartDateTime.plusDays(1));
+        verify(event).setEndDateTime(oldEndDateTime.plusDays(1));
+        verify(event).save();
+    }
+
+    @Test
+    public void testExecute_shiftingEventTask_onlyTimeSpecified_successful() throws Exception {
+        LocalDateTime oldStartDateTime = LocalDateTime.now();
+        LocalDateTime oldEndDateTime = oldStartDateTime.plusHours(1);
+
+        TaskAttributes event = mock(TaskAttributes.class);
+        when(event.getStartDateTime()).thenReturn(oldStartDateTime);
+        when(event.getEndDateTime()).thenReturn(oldEndDateTime);
+        when(ui.getTaskId(4)).thenReturn(4);
+        when(taskDb.getById(4)).thenReturn(event);
+
+        LocalDateTime shiftedDateTime = oldStartDateTime.plusDays(1);
+
+        RescheduleTaskCommand command = new RescheduleTaskCommand.Builder()
+            .setId(4)
+            .setShiftedDateTime(shiftedDateTime)
+            .setShiftedDateSpecified(false)
+            .setShiftedTimeSpecified(true)
+            .build();
+
+        command.execute();
+
+        verify(event).setStartDateTime(oldStartDateTime);
+        verify(event).setEndDateTime(oldEndDateTime);
+        verify(event).save();
     }
 
     @Test

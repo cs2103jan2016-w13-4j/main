ed44f789e12da307b1a3db076ff7ade295d46c38
Thng Kai Yuan <hedonist.ky@gmail.com>
2016-04-11 22:56:44 +0800

    Resolve checkstyle issue

--- src/main/java/jfdi/storage/entities/Task.java
+++ src/main/java/jfdi/storage/entities/Task.java
@@ -20,7 +20,8 @@ public class Task {
     private LocalDateTime endDateTime = null;
     private boolean isCompleted = false;
 
-    public Task(Integer id, String description, LocalDateTime startDateTime, LocalDateTime endDateTime, boolean isCompleted) {
+    public Task(Integer id, String description, LocalDateTime startDateTime, LocalDateTime endDateTime,
+            boolean isCompleted) {
         this.id = id;
         this.description = description;
         this.startDateTime = startDateTime;
